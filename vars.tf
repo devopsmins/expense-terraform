bf6c6630c6c13e18cd25ba7056a66cdb64cbdb0d
minhaj <minhajfathima1023@gmail.com>
2024-02-15 06:04:28 -0500

    Prometheus ansible change

--- vars.tf
+++ vars.tf
@@ -15,4 +15,13 @@ variable "rds_engine" {}
 variable "rds_engine_version" {}
 variable "rds_instance_class" {}
 #variable "kms" {}
+variable "backend" {}
+#variable "bastion_cidrs" {}
+#variable "frontend" {}
+#variable "public_alb" {}
+#variable "backend_alb" {}
+#variable "route53_zone_id" {}
+#variable "certificate_arn" {}
+#variable "prometheus_cidrs" {}
+
 
